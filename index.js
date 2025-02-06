e97474045aa0045d65b28c153a5284cf2b26a707
spanheim <139687820+spanheim@users.noreply.github.com>
2025-02-06 17:52:58 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,6 @@
 //-----------------------------------------------------------------------------
 
 exports.helloWorld = (req, res) => {
-  const message="<font color='blue'>СloudFunction of Denis Astahov!</font><br><b>App Version 2.2</b>";
+  const message="<font color='blue'>СloudFunction of Denis Astahov!</font><br><b>App Version 2.3</b>";
   res.status(200).send(message);
 };
